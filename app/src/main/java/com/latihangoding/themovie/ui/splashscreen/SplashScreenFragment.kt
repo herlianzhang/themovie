779377f34568d217cbc533bc7e148946df47e7cb
herlianzhang <herlianzhang370@gmail.com>
2020-11-04 16:39:55 +0700

    add splashscreen

---
+++ app/src/main/java/com/latihangoding/themovie/ui/splashscreen/SplashScreenFragment.kt
@@ -0,0 +1,28 @@
+package com.latihangoding.themovie.ui.splashscreen
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.latihangoding.themovie.R
+
+class SplashScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler(Looper.myLooper()!!).postDelayed({
+            findNavController().navigate(R.id.action_splashScreenFragment_to_homeFragment)
+        }, 3000)
+    }
+}
